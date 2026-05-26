05ae030be618b0e2308dc5f93fcaba68d83d3ce1
Kevin Najarro <nkevinmanuel94@gmail.com>
2026-05-25 19:21:43 -0600

    fix: navbar overlap on mobile and narrow book grid
    Restore body padding-top in App.css with !important so Bulma's CSS
    reset doesn't remove the offset needed for the fixed navbar.
    Widen book cards to full width on mobile (is-full-mobile).

--- src/components/BookGrid.tsx
+++ src/components/BookGrid.tsx
@@ -8,7 +8,7 @@ function BookGrid({ books }: BookGridProps) {
   return (
     <div className="columns is-multiline">
       {books.map((book) => (
-        <div key={book.key} className="column is-3-desktop is-4-tablet is-6-mobile">
+        <div key={book.key} className="column is-3-desktop is-4-tablet is-full-mobile">
           <BookCard book={book} />
         </div>
       ))}
